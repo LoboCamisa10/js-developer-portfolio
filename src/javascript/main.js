b59d7b70dd68bf1cc0da8a833c7282ca7740dc2a
guto2204 <guto2204@gmail.com>
2024-03-27 21:25:58 -0300

    Adicionando uma introudção sobre mim

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,12 +3,15 @@ function updateProfilePhoto(profileData){
     photo.src = profileData.photo
     photo.alt = profileData.name
 
-    const name = document.getElementById('phofile.name')
+    const name = document.getElementById('profile.name')
     name.innerText = profileData.name
     
     const job = document.getElementById('profile.job')
     job.innerText = profileData.job
 
+    const location = document.getElementById('profile.location')
+    location.innerText  = profileData.location
+    
 }
 
 function updateProfileName(profileData){
@@ -17,5 +20,5 @@ function updateProfileName(profileData){
 
 (async ()=>{
     const profileData = await FetchProfileData()
-    console.log(profileData)
+    updateProfilePhoto(profileData)
 })()

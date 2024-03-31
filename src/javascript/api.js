88d31933f7ec8a43fbf3674b4b27935fc7621403
guto2204 <guto2204@gmail.com>
2024-03-31 17:14:11 -0300

    Corrigindo o path da API

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -1,5 +1,5 @@
 async function FetchProfileData(){
-        const url = 'https://raw.githubusercontent.com/LoboCamisa10/js-developer-portfolio/master../data/profile.json';
+        const url = 'https://raw.githubusercontent.com/LoboCamisa10/js-developer-portfolio/master/src/data/profile.json';
         const fetching = await fetch(url)
         return await fetching.json()
 }

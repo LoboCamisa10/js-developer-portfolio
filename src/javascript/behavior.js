46bae8d2cfd610559bb5352d6dad480bdfb4b4f7
guto2204 <guto2204@gmail.com>
2024-03-31 16:15:23 -0300

    Finalizado.

--- src/javascript/behavior.js
+++ src/javascript/behavior.js
@@ -1,17 +1,17 @@
 // Abaixo tem a função de abrir e fechar a "introdução" com cliques.
 
-const littleHistoryTrigger = document.querySelectorAll('.trigger')
+const AllTrigger = document.querySelectorAll('.trigger')
 
-littleHistoryTrigger.forEach((trigger) => {
+AllTrigger.forEach((trigger) => {
     trigger.addEventListener('click', (e) =>{
 
-        const littleHist = trigger.parentElement
-        const isOpen = littleHist.classList.contains('open')
+        const paiDoTrigger = trigger.parentElement
+        const isOpen = paiDoTrigger.classList.contains('open')
 
         if (isOpen){
-            littleHist.classList.remove('open')            
+            paiDoTrigger.classList.remove('open')            
         } else{
-            littleHist.classList.add('open')
+            paiDoTrigger.classList.add('open')
         }
     })
 });
